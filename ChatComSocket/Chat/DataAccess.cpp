b1e71ef9a753b8c62a9f6084eedd4df9d3c195b9
João Antonio Martins Filho <joaoantoniomartinsfilho@gmail.com>
2018-10-04 21:01:31 -0300

    Commits da hora

--- ChatComSocket/Chat/DataAccess.cpp
+++ ChatComSocket/Chat/DataAccess.cpp
@@ -1,4 +1,5 @@
 #include "DataAccess.h"
+#include "Util.h"
 
 
 
@@ -12,11 +13,58 @@ DataAccess::DataAccess(std::string userName, std::string userPassword, std::stri
 	this->_port = serverPort;
 
 	connect = mysql_init(NULL);
-	connect = mysql_real_connect(connect, _serverAddress.c_str(), _userDb.c_str(),
-		_passwordDb.c_str(), _dataBaseName.c_str(), _port, NULL, 0);
+
+	/*connect = mysql_real_connect(connect, _serverAddress.c_str(), _userDb.c_str(),
+		_passwordDb.c_str(), _dataBaseName.c_str(), _port, NULL, 0);*/
 }
 
 
-DataAccess::~DataAccess()
+DataAccess::DataAccess()
 {
 }
+
+DataAccess::~DataAccess()
+{	
+	if (this->connect != NULL) {
+		mysql_close(this->connect);
+	}
+
+}
+
+bool DataAccess::CreateNewUser(std::string newUserName, std::string newUserPassword, std::string newUserLogin)
+{
+	try {
+
+		this->connect = mysql_real_connect(connect, _serverAddress.c_str(), _userDb.c_str(),
+			_passwordDb.c_str(), _dataBaseName.c_str(), _port, NULL, 0);
+		
+		if (!connect)
+		{
+			return false;
+		}		
+
+		std::string query = "INSERT INTO user VALUES ('" + newUserName + "', '" + newUserLogin + "', '" + newUserPassword + "')";
+
+		bool created = mysql_query(this->connect, query.c_str());  
+		
+		if (created)
+			return true;
+	}
+	catch (...) {		
+		mysql_close(this->connect);
+		return false;
+	}
+
+	return false;
+}
+
+DataAccess * DataAccess::GetInstance()
+{
+	DataAccess * newDbObj = new DataAccess("root", "", "chat", "127.0.0.1", 3306);
+		
+	return newDbObj;
+}
+
+
+
+
